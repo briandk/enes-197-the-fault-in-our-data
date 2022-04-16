63dc677619167cef7b017a42bfe72a93fc8e082b
Brian Danielak <briandaniela.k+github@gmail.com>
2022-04-15 23:03:29 -0400

    Remove `devtools` dependency

--- _install_dependencies.R
+++ _install_dependencies.R
@@ -1,7 +1,6 @@
 # Necessary for `bookdown::bs4_book()` output format
 install.packages(c("bookdown",
                    "bslib",
-                   "devtools",
                    "downlit",
                    "reticulate",
                    "xml2"))
