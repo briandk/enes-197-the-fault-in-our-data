4e30fafb6bf87bfd6910ef76049fc8f1f990dbff
Brian A. Danielak <briandk@users.noreply.github.com>
2022-05-20 14:30:14 -0400

    Install bookdown from `main` branch
    Explicitly install bookdown from the "main" branch

--- _install_dependencies.R
+++ _install_dependencies.R
@@ -1,6 +1,6 @@
 # bslib is necessary for `bookdown::bs4_book()` output format
 install.packages(c("remotes"))
-remotes::install_github("rstudio/bookdown")
+remotes::install_github("rstudio/bookdown", "main")
 
 install.packages(c(
                    "bslib",
