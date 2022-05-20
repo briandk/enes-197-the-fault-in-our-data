aec1cac1bc81d4c3a33d57f99495ba025b99dd55
Brian A. Danielak <briandaniela.k+github@gmail.com>
2022-05-20 15:36:11 -0400

    Stop using renv

---
+++ _install_R_dependencies.R
@@ -0,0 +1,10 @@
+# bslib, downlit, and xml2 are necessary for bookdown
+
+install.packages(c("bookdown",
+                   "bslib",
+                   "downlit",
+                   "dplyr",
+                   "magrittr",
+                   "reticulate",
+                   "tidyverse",
+                   "xml2"))
