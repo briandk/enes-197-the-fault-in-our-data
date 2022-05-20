4ee934896cd01f984e517ad66c4a3c1a11b4c521
Brian A. Danielak <briandaniela.k+github@gmail.com>
2022-05-20 14:48:07 -0400

    Fix bookdown install
    - Make sure `curl` gets installed as an R dependency
    - Use `@[ref]` specifier to show which branch we want to install bookdown from

--- _install_dependencies.R
+++ _install_dependencies.R
@@ -1,6 +1,6 @@
-# bslib is necessary for `bookdown::bs4_book()` output format
-install.packages(c("remotes"))
-remotes::install_github("rstudio/bookdown", "main")
+# I believe bslib and downlit are necessary for `bookdown::bs4_book()` output format
+install.packages(c("curl", "remotes"))
+remotes::install_github("rstudio/bookdown@main")
 
 install.packages(c(
                    "bslib",
